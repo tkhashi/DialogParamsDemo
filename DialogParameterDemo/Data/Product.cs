173c11c61be2335393cd02e8efe00349f80b6030
KazuhiroTakahashi <k_takahashi@just-ltd.co.jp>
2021-11-28 18:29:19 +0900

    Add just grid

---
+++ DialogParameterDemo/Data/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace DialogParameterDemo.Data
+{
+    public class Product
+    {
+        public string Id { get; }
+        public string Name { get; }
+        public int Price { get; }
+
+        public Product(string id, string name, int price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+    }
+}
---
+++ DialogParameterDemo/MainWindowViewModel.cs
@@ -0,0 +1,20 @@
+﻿using DialogParameterDemo.Data;
+using System.Collections.Generic;
+
+namespace DialogParameterDemo
+{
+    public class MainWindowViewModel
+    {
+        public IList<Product> Products { get; }
+
+        public MainWindowViewModel()
+        {
+            Products = new List<Product>();
+            Products.Add(new Product("1", "梅干し", 300));
+            Products.Add(new Product("2", "納豆", 400));
+            Products.Add(new Product("3", "甘納豆", 500));
+            Products.Add(new Product("4", "甘酒", 600));
+            Products.Add(new Product("5", "煮干し", 700));
+        }
+    }
+}
